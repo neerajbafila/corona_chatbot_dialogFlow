4abc9b008a030457ccfc13b003c8802d28b36395
Neeraj Bafila <58370152+neerajbafila@users.noreply.github.com>
2020-04-15 15:14:54 +0530

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -51,10 +51,10 @@ class EmailSender():
 
         # Converts the Multipart msg into a string
         self.text = self.msg.as_string()
-        self.text = """Test msg"""
+        #self.text = """Test msg"""
 
         # sending the mail
-        self.smtp.sendmail(self.msg['From'], user_email, self.text)
+        self.smtp.sendmail(self.msg['From'], self.msg['To'], self.text)
 
 
         # terminating the session
